cf2e46a166a2000e94e2c1335b2cc1e80b7233d0
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-02 22:41:05 -0700

    Fix minute and hour degree calculation.

--- js/main.js
+++ js/main.js
@@ -6,17 +6,17 @@ const setDate = () => {
     // second
     const now = new Date();
     const seconds = now.getSeconds();
-    const secondsDegrees = seconds * 6;
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
     // minute
     const minutes = now.getMinutes();
-    const minutesDegree = minutes * 6;
+    const minutesDegree = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
     minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
 
     //hour
     const hours = now.getHours();
-    const hoursDegree = hours * 30;
+    const hoursDegree = ((hours / 12) * 360) + ((minutes / 60) * 6) + 90;
     hourHand.style.transform = `rotate(${hoursDegree}deg)`;
 
     [secondHand, minuteHand, hourHand].forEach(element =>
